bb1222afe7d59eb9b0a34e94652d18ab70b97b18
Nguyen Duy <bdtren1@gmail.com>
2021-09-26 17:51:12 +0700

    setup viewBinding, ConstaintLayout add choose keyboard setting

--- android/app/src/main/java/com/reactkeyboard/ime/ImeMainActivity.java
+++ android/app/src/main/java/com/reactkeyboard/ime/ImeMainActivity.java
@@ -3,6 +3,7 @@ package com.reactkeyboard.ime;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +17,18 @@ public class ImeMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ime_main);
 
         Button btnInputSetting = findViewById(R.id.btnInputSetting);
+        Button btnSetKeyboard = findViewById(R.id.btnSetKeyboard);
         btnInputSetting.setOnClickListener(l -> {
             Intent intent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
             startActivity(intent);
         });
+
+        btnSetKeyboard.setOnClickListener(l -> {
+            InputMethodManager imeManager = (InputMethodManager)
+                    getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+            imeManager.showInputMethodPicker();
+        });
+
     }
 
 }
